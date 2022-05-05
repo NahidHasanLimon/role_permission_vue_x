2b57640b5057086959d098a8a63ee2d130e51f16
NahidHasanLimon <nh.limon@yahoo.com>
2022-05-06 03:40:29 +0600

    import vue route and pass app instance in vue router during use

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import {router} from './router'
+import {createRouter} from './router'
 import store from './store'
 import App from './App.vue'
 import access from './access'
@@ -8,14 +8,14 @@ import axios from 'axios'
 
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
-
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'http://localhost:8000/'
 
 store.dispatch('auth/me').then(() => {
     const app = createApp(App)
     app.use(store)
-    app.use(router,createApp)
+    const router = createRouter(app)
+    app.use(router)
     app.use(access)
     app.use(VueSweetalert2)
     app.mount('#app')
